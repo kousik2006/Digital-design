d89c11bbc5b42b667959c45c047c457ccfd45712
Kousik2006 <karkousik457@gmail.com>
2025-12-16 09:09:53 +0530

    half_subtractor done

---
+++ Day 006___half adder and half subtractor/half_subtractor/half_subtractor_tb.v
@@ -0,0 +1,38 @@
+`timescale 1ns/1ps
+
+module half_subtractor_tb;
+
+    reg a,b,clk;
+    wire diff,borrow;
+
+    initial clk = 1'b0;
+    always #5 clk = ~clk;
+
+    half_adder dut(
+        .a(a),
+        .b(b),
+        .diff(diff),
+        .borrow(borrow)
+    );
+
+    initial begin
+        $dumpfile("half_subtractor_tb.vcd");
+        $dumpvars(0,half_subtractor_tb);
+
+        a = 1'b0; b = 1'b0;
+        #10 a = 1'b0; b = 1'b1;
+        #10 a = 1'b1; b = 1'b0;
+        #10 a = 1'b1; b = 1'b1;
+        #10 $finish;
+
+    end
+
+    always @(posedge clk) begin
+        $display ("a = %b , b = %b, diff = %b, borrow = %b",a,b,diff,borrow);
+
+    end
+
+    
+
+
+endmodule
