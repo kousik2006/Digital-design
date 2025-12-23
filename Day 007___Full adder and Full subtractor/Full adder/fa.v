509d2f6f25d561fe5de23c5db76db87c4961a58c
Kousik2006 <karkousik457@gmail.com>
2025-12-23 15:07:46 +0530

    full adder

---
+++ Day 007___Full adder and Full subtractor/Full adder/fa.v
@@ -0,0 +1,11 @@
+`timescale 1ns/1ps
+
+module full_adder(
+    input a,b,carry_in,
+    output sum,carry_out
+)
+
+    assign sum = a ^ b ^ carry_in;
+    assign carry_out = (a & b) | (carry_in & (a ^ b));
+
+endmodule
