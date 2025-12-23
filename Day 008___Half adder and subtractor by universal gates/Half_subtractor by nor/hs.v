e572411b87a48d260089dc9c61ea4d8e2a32559f
Kousik2006 <karkousik457@gmail.com>
2025-12-23 21:31:23 +0530

    done...

--- Day 008___Half adder and subtractor by universal gates/Half_subtractor by nor/hs.v
+++ Day 008___Half adder and subtractor by universal gates/Half_subtractor by nor/hs.v
@@ -7,10 +7,11 @@ module half_subtractor(
 
 wire t1,t2,t3;
 
-nand g1(t1,a,b);
-nand g2(t2,a,t1);
-nand g3(t3,t1,b);
-nand DIFF(diff,t2,t3);
-nand BORROW(borrow,t3,t3);
+nor g1(t1,a,b);
+nor g2(t2,b,t1);
+nor BORROW(borrow,t1,a);
+nor g3(t3,t2,borrow);
+nor DIFF(diff,t3,t3);
+
 
 endmodule
