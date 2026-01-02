fc68e1e4b34faf200f063488b1e94246acb2f074
Kousik2006 <karkousik457@gmail.com>
2026-01-03 02:50:32 +0530

    odd parity chcker done

---
+++ Day 006__Even,odd_parity_generator_checker/odd_parity_checker/odd_parity_checker.v
@@ -0,0 +1,11 @@
+module odd_parity_checker #(
+    parameter N = 7 // data width = N+1
+)(
+    input wire [N:0]data,
+    input wire parity_in,
+    output wire error
+);
+
+    assign error = (^{data,parity_in} == 1'b0);
+
+endmodule
